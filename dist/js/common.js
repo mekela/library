011edc2f3578d16fc5a6e875114a0e37711e27c1
Alex Gerasimyuk <2011.sanya@gmail.com>
2019-03-22 00:02:40 +0200

    add index

---
+++ dist/js/common.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	//slider
+	$('.book__slider').slick();
+	//menu
+	$( ".nav__trigger button" ).click(function() {
+		$( ".nav" ).addClass( "active" );
+	});
+	$( ".nav__close button" ).click(function() {
+		$( ".nav" ).removeClass( "active" );
+	});
+});
